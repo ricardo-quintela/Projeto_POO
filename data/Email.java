0c1935c79fe79e43782012b01d15ce9eafb92938
Ricardo Quintela <69524535+FiFcreeN@users.noreply.github.com>
2021-11-19 14:44:51 +0000

    Add files via upload

---
+++ data/Email.java
@@ -0,0 +1,67 @@
+package data;
+
+public class Email {
+    private String endereco;
+    private String domain;
+
+    public Email() {}
+
+
+    /**
+     * Constructor
+     *
+     * @param endereco the e-mail address
+     */
+    public Email(String endereco) {
+        this.endereco = endereco;
+        this.domain = genDomain(endereco);
+    }
+
+
+    /**
+     * Constructor
+     *
+     * @param endereco the e-mail address
+     * @param domain the domain name
+     */
+    public Email(String endereco, String domain) {
+        this.endereco = endereco;
+        this.domain = domain;
+    }
+
+
+    /**
+     * Generates the domain of an email address
+     *
+     * @param endereco the email address
+     * @return the domain of the email address
+     */
+    public String genDomain(String endereco){
+        int i = endereco.indexOf('@');
+
+        return endereco.substring(i);
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+
+    @Override
+    public String toString() {
+        return endereco;
+    }
+}
