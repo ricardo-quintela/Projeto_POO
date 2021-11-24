369590a26ea41930752a2b0323ee9404215af876
Ricardo Quintela <69524535+FiFcreeN@users.noreply.github.com>
2021-11-24 16:38:21 +0000

    Adicionei produtos de Mobiliario

---
+++ products/Mobiliario.java
@@ -0,0 +1,49 @@
+package products;
+
+public class Mobiliario extends Produto{
+    private float peso;
+    private float[] dimensao;
+
+    /**
+     * Default constructor
+     */
+    public Mobiliario() {}
+
+
+    /**
+     * Constructor
+     * @param peso the weight of the product
+     * @param dimensao the size of the product
+     */
+    public Mobiliario(float peso, float[] dimensao) {
+        this.peso = peso;
+        this.dimensao = dimensao;
+    }
+
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float[] getDimensao() {
+        return dimensao;
+    }
+
+    public void setDimensao(float[] dimensao) {
+        this.dimensao = dimensao;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " que pesa " + peso +
+                "kg e mede " + dimensao[0] + "m de comprimento, "
+                + dimensao[1] + "m de largura e " +
+                dimensao[2] + "m de altura";
+    }
+}
